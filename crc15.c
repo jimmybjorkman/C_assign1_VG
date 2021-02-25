969c60695c0e6ff65f055ea72c09c77a017ca856
jimmybjorkman <jimmybjorkman@live.se>
2021-02-25 16:32:04 +0100

    Clean up and written with function call

--- crc15.c
+++ crc15.c
@@ -3,37 +3,40 @@
 
 #define POLYNOMIAL 0xC599U // The CAN protocol uses the CRC-15 with this polynomial
 #define FIRSTBITSET (1 << 15) //
-int main(void)
+
+uint16_t calc_CRC(uint8_t message[], uint8_t length){
+    uint16_t CRC = 0x0000U;
+
+    for (int character = 0; character < (length); character++)
+    {
+        for (int bit = 0; bit < 8; bit++)
+        {
+            CRC = CRC << 1;
+            CRC = CRC | ((message[character] >> bit) & 1);
+            if (CRC & FIRSTBITSET)
+            {
+                CRC = CRC ^ POLYNOMIAL;
+            }
+        }
+    }
+    return CRC;
+}
+
+    int main(void)
 {
     //uint8_t message[] = {'A', 'B', 0, 0};
     uint8_t message[] = {'H', 'e','l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', 0, 0}; // 15 zeros have been appended to the message
     uint8_t length = sizeof(message) / sizeof(message[0]);
-    uint8_t kvot = 0x00U;
 
-    uint16_t CRC = 0x0000U;
+    uint16_t CRC = calc_CRC(message, length);
 
-    for (int character = 0; character < (length);character++)
-    {  
-        for (int bit = 0; bit < 8; bit++)
-        {
-            CRC = CRC | ((message[character] >> bit) & 1);
-            if(CRC & FIRSTBITSET)
-            {
-                CRC = CRC ^ POLYNOMIAL;
-            }
-            CRC = CRC << 1;
-        }
-    }
-
-    printf("CRC is %04x\n and CRC shifte one is %04x\n", CRC, CRC >> 1);
-    CRC = CRC >> 1;
+    printf("CRC is %04x\n", CRC);
     uint16_t revCRC = 0x0000U;
 
     for (int bit = 8; bit < 16; bit++)
     {
         revCRC = revCRC << 1;
-        revCRC = revCRC | ((CRC >> bit) & 1);
-        
+        revCRC = revCRC | ((CRC >> bit) & 1); 
     }
     printf("reverse CRC first is %04x\n", revCRC);
     for (int bit = 0; bit < 8; bit++)
@@ -46,30 +49,16 @@ int main(void)
     message[length - 2] =     (uint8_t)(revCRC >> 8);
     message[length - 1] =     (uint8_t)(revCRC); 
 
-    message[1] = 'a';
+    //message[1] = 'a';
 
         for (int character = 0; character < (length); character++)
     {
         printf("%d: %02x\n", character, message[character]);
     }
     
-    // message[1] = 'a';
-
     // Validate the message.
-    CRC = 0x0000U;
-    
-    for (int character = 0; character < (length); character++)
-    {
-        for (int bit = 0; bit < 8; bit++)
-        {
-            CRC = CRC | ((message[character] >> bit) & 1);
-            if (CRC & FIRSTBITSET)
-            {
-                CRC = CRC ^ POLYNOMIAL;
-            }
-            CRC = CRC << 1;
-        }
-    }
+    CRC = CRC = calc_CRC(message, length);
+
     printf("CRC is %04x\n", CRC);
 
     return 0;
